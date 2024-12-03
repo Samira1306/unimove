a1f1bb8135959ff07e44d9e013a619188ee86aa6
Juan Diego Ramirez Muñoz <orugelesr@icetex.gov.co>
2024-12-02 21:40:43 -0500

    Update importaciones

--- Client/android/app/src/Services/RouteService.ts
+++ Client/android/app/src/Services/RouteService.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { Route } from '../Models/RouteModel';
+import { Vehicle } from '../Models/VehicleModel';
 
 const BASE_URL = 'http://10.0.2.2:3000';
 
@@ -8,9 +9,22 @@ export const createRoute = async (route: Route) => {
     console.log(route);
     const response = await axios.post(`${BASE_URL}/routes`, route);
     console.log('Respuesta del servidor:', response.data); 
+    const response2 = await getVehiclesByUser('67351210581a6a4f04f5ce0e');
+    console.log('Respuesta del servidor 2:',response2);
     return response.data;
   } catch (error) {
     throw new Error('Error al crear la ruta');
+  }
+};
+
+export const getVehiclesByUser = async (userId: string): Promise<Vehicle[]> => {
+  try {
+    const response = await axios.get(`${BASE_URL}/vehicles/users/${userId}`);
+    console.log('Respuesta del servidor:', response.data);
+    return response.data; // Retorna los vehículos
+  } catch (error) {
+    console.error('Error al obtener los vehículos:', error);
+    throw new Error('Error al obtener los vehículos');
   }
 };
 
